44223a66ab71ad5a2a16279f3e25e9584138cdec
Archana Parasnis <arch.par@gmail.com>
2017-05-16 18:08:22 -0400

    make it better

--- test/app.js
+++ test/app.js
@@ -6,12 +6,21 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
+    // if(process.env.MLAB_USERNAME) {
+    //     connectionString = process.env.MLAB_USERNAME + ":" +
+    //         process.env.MLAB_PASSWORD + "@" +
+    //         process.env.MLAB_HOST + ':' +
+    //         process.env.MLAB_PORT + '/' +
+    //         process.env.MLAB_APP_NAME;
+    // }
+
+    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+        var password = process.env.MLAB_PASSWORD_WEBDEV;
+        connectionString = 'mongodb://' + username + ':' + password;
+        connectionString += 'ds137261.mlab.com:37261/heroku_lbm56696'; // user yours
+
     }
 
     var mongoose = require("mongoose");
