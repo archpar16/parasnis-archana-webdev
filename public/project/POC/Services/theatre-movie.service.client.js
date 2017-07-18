a9cda1a01a08eefaa74d36d8e554f41cc5a2cacf
Amit Paranjape <asparanjape@Amits-MacBook-Pro.local>
2017-07-18 06:06:39 -0400

    send the api requests over https since heroku loads as https

--- public/project/POC/Services/theatre-movie.service.client.js
+++ public/project/POC/Services/theatre-movie.service.client.js
@@ -12,29 +12,29 @@
         };
 
         function searchTheatres(zipcode) {
-            var baseURL = "http://data.tmsapi.com/v1.1/";
+            var baseURL = "https://data.tmsapi.com/v1.1/";
             var apiKey = "api_key=ffs4rxcjp3wcfveqmfuf8f62";
             var url = baseURL+"theatres?zip="+zipcode+"&"+apiKey;
             console.log('theatres find url = '+ url);
-            return $https.get(url);
+            return $http.get(url);
         }
 
         function searchTheatreDetails(theatreId) {
-            var baseURL = "http://data.tmsapi.com/v1.1/";
+            var baseURL = "https://data.tmsapi.com/v1.1/";
             var apiKey = "api_key=ffs4rxcjp3wcfveqmfuf8f62";
             var url = baseURL+"theatres/"+theatreId+"?"+apiKey;
             console.log('theatre details url = '+ url);
-            return $https.get(url);
+            return $http.get(url);
         }
 
         function searchMovies(theatreId) {
-            var baseURL = "http://data.tmsapi.com/v1.1/";
+            var baseURL = "https://data.tmsapi.com/v1.1/";
             var apiKey = "api_key=ffs4rxcjp3wcfveqmfuf8f62";
             var today = $filter('date')(new Date(), 'yyyy-MM-dd');
             console.log('now =' + today);
             console.log('theatreId =' + theatreId);
             var url = baseURL + "theatres/"+theatreId+"/showings?startDate="+today+"&"+apiKey;
-            return $https.get(url);
+            return $http.get(url);
         }
     }
 })();
